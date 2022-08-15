82a91184c84b873b14d7a2f4584cbf8023e00502
ittehadzihan <zihan991@gmail.com>
2022-08-14 20:29:20 -0400

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.textContent = "YOUR-NAME is the champion"
